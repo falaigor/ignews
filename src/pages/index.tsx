079391b656c2fdadc186b115ca34c496f9123bdc
Igor Santos <falaigors@gmail.com>
2021-03-27 09:25:59 -0300

    Estilos Globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import styles from '../styles/home.module.scss';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Hello World
-    </h1>
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+    </>
   );
 }
