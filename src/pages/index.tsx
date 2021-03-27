8505a8be4c4e60554fd2682e9e00bb525fe7b240
Igor Santos <falaigors@gmail.com>
2021-03-27 10:10:13 -0300

    Criaçao Pagina Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,25 @@
-import styles from '../styles/home.module.scss';
+import styles from './home.module.scss';
 import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   );
 }
