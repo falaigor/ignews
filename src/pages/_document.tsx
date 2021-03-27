c0094564cf1a29e65ab98c8751a5dfff189e3c48
Igor Santos <falaigors@gmail.com>
2021-03-27 09:12:41 -0300

    Configurando _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,8 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
           <title>ig.news</title>
         </Head>
         <body>
